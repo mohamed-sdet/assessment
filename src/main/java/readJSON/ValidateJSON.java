e1338250621c91407a58135a256dc7ccf49712c6
Mohamed <Mohamed@DESKTOP-2OPMLDB>
2023-01-21 07:05:04 +0530

    Get Player Names List

--- src/main/java/readJSON/ValidateJSON.java
+++ src/main/java/readJSON/ValidateJSON.java
@@ -1,18 +1,40 @@
 package readJSON;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Test;
 
 public class ValidateJSON {
 	
-	public void ValidateTest01() throws FileNotFoundException
+	@Test
+	public void ValidateTest01() throws IOException, ParseException
 	{
 		
 		JSONParser jsonParser = new JSONParser();
 		
 		FileReader reader = new FileReader(".\\testdata\\inputData\\TeamRCB.json");
+		
+		Object obj = jsonParser.parse(reader);
+		
+		JSONObject  playersobj = (JSONObject)obj;
+		
+		String playerName = (String)playersobj.get("name");
+		String countryName = (String)playersobj.get("country");
+		JSONArray players = (JSONArray)playersobj.get("player");
+		
+		for(int i=0;i<players.size();i++)
+		{
+			JSONObject player = (JSONObject)players.get(i);
+			String playerNames = (String) player.get("name");
+			System.out.println(playerNames);
+		}
+		
+		
 	}
 
 	
